6b4c98dde0311057a5f3ff2df34be5ddb80a31e9
Sven H <sven.hjol@protonmail.com>
2024-12-17 22:03:32 +0000

    README and update text

---
+++ README.md
@@ -0,0 +1,5 @@
+![](https://raw.githubusercontent.com/GirafiStudios/Waddles/refs/heads/master/common/src/main/resources/assets/waddles/logo.png)
+
+This is a port of [Waddles](https://github.com/GirafiStudios/Waddles) by GirafiStudios to work with the Charmony suite of mods on fabric.
+
+More information and downloads for the original project can be found on [CurseForge](https://minecraft.curseforge.com/projects/waddles)
--- src/main/java/svenhjol/charmony/waddles/WaddlesMod.java
+++ src/main/java/svenhjol/charmony/waddles/WaddlesMod.java
@@ -3,9 +3,10 @@ package svenhjol.charmony.waddles;
 import net.minecraft.resources.ResourceLocation;
 import svenhjol.charmony.core.annotations.ModDefinition;
 import svenhjol.charmony.core.base.Mod;
+import svenhjol.charmony.core.enums.Side;
 
 @ModDefinition(
-    id = WaddlesMod.ID, name = "Waddles", description = "Port of Waddles by girafi", sides = {})
+    id = WaddlesMod.ID, name = "Waddles", description = "Port of Waddles by GirafiStudios", sides = {Side.Client, Side.Common})
 public class WaddlesMod extends Mod {
     public static final String ID = "charmony-waddles";
     private static WaddlesMod instance;
