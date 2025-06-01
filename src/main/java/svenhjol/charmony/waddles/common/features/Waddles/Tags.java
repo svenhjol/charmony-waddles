472e3bee4901aa8aa35c3b567c007b8e9d7365aa
Sven H <sven.hjol@protonmail.com>
2025-06-01 20:05:56 +0100

    Update for 1.21.6. 1.6.0

--- src/main/java/svenhjol/charmony/waddles/common/features/Waddles/Tags.java
+++ src/main/java/svenhjol/charmony/waddles/common/features/Waddles/Tags.java
@@ -1,12 +1,11 @@
 package svenhjol.charmony.waddles.common.features.Waddles;
 
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
-import svenhjol.charmony.waddles.WaddlesMod;
+import svenhjol.charmony.core.Charmony;
 
 public final class Tags {
     public static final TagKey<Block> PENGUIN_SPAWNABLE = blockTag("penguin_spawnable");
@@ -14,14 +13,14 @@ public final class Tags {
     public static final TagKey<Biome> SPAWNS_PENGUINS = biomeTag("spawns_penguins");
 
     public static TagKey<Block> blockTag(String name) {
-        return TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(WaddlesMod.ID, name));
+        return TagKey.create(Registries.BLOCK, Charmony.id(name));
     }
 
     public static TagKey<Biome> biomeTag(String name) {
-        return TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath(WaddlesMod.ID, name));
+        return TagKey.create(Registries.BIOME, Charmony.id(name));
     }
 
     public static TagKey<Item> itemTag(String name) {
-        return TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(WaddlesMod.ID, name));
+        return TagKey.create(Registries.ITEM, Charmony.id(name));
     }
 }
